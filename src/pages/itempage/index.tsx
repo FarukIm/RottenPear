99bc17abb851b25e946b3ce89c11520ee54cde87
FarukIm <f.imamovic27@gmail.com>
2022-12-14 23:32:13 +0100

    finished styling itempage

--- src/pages/itempage/index.tsx
+++ src/pages/itempage/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 import Header from "../../components/header";
 
