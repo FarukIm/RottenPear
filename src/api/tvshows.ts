bfd67bc37466ab1dcbb8ed4840d29c5a6d9af397
FarukIm <f.imamovic27@gmail.com>
2024-05-09 14:19:39 +0200

    changed tvshows api requests to use axios

--- src/api/tvshows.ts
+++ src/api/tvshows.ts
@@ -1,26 +1,45 @@
+import axios from 'axios';
+
 const getTopShows = async () => {
-	const response = await fetch(
-		"https://api.themoviedb.org/3/tv/top_rated?api_key=c71c4cfcca0d03f577a5e60d21141c58&language=en-US&page=1"
-	);
-	let data = await response.json();
-	data = data.results.slice(0, 10);
-	return data;
+	const options = {
+		method: 'GET',
+		url: `tv/top_rated?language=en-US&page=1`,
+	};
+	try {
+		const { data: response } = await axios.request(options);
+		return response.results.slice(0, 10);
+	} catch (e) {
+		console.log(e);
+		return e;
+	}
 };
 
 const getSearchShows = async (term: string) => {
-	const response = await fetch(
-		`https://api.themoviedb.org/3/search/tv?api_key=c71c4cfcca0d03f577a5e60d21141c58&language=en-US&query=${term}&page=1&include_adult=false`
-	);
-	const data = await response.json();
-	return data;
+	const options = {
+		method: 'GET',
+		url: `search/tv?query=${term}&language=en-US&page=1&include_adult=false`,
+	};
+	try {
+		const { data: response } = await axios.request(options);
+		return response.results;
+	} catch (e) {
+		console.log(e);
+		return e;
+	}
 };
 
 const getShow = async (id: string | undefined) => {
-	const response = await fetch(
-		`https://api.themoviedb.org/3/tv/${id}?api_key=c71c4cfcca0d03f577a5e60d21141c58&language=en-US`
-	);
-	const data = await response.json();
-	return data;
+	const options = {
+		method: 'GET',
+		url: `tv/${id}?language=en-US`,
+	};
+	try {
+		const { data: response } = await axios.request(options);
+		return response;
+	} catch (e) {
+		console.log(e);
+		return e;
+	}
 };
 
 export { getTopShows, getSearchShows, getShow };
